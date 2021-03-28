ed3a05fc7e8b3da14fc6dd47b3434db252799b5c
Thomas Nguyen <thomasngn673@gmail.com>
2021-03-27 19:32:07 -0500

    add countFrequency and more configuration

--- tabSetup/analyticsTab.py
+++ tabSetup/analyticsTab.py
@@ -5,8 +5,8 @@ from matplotlib.figure import Figure
 import pandas as pd
 import numpy as np
 
-df_17_18 = pd.ExcelFile('IEEE_Member_App_17-18.xlsx') # work on reading all .xlsx files in directory (looping)
-df_20_21 = pd.ExcelFile('IEEE_Member_App_20-21.xlsx')
+xl_17_18 = pd.ExcelFile('IEEE_Member_App_17-18.xlsx') # work on reading all .xlsx files in directory (looping)
+xl_20_21 = pd.ExcelFile('IEEE_Member_App_20-21.xlsx')
 
 def setup(self):
     # self is of the Tab class
@@ -14,7 +14,7 @@ def setup(self):
     # self.tabName - the name of the tab
     # self.displayFrame - the root frame to build all content inside
 
-    # defop (DEFault OPtion): adds 'Select year' or 'Select event' to beginning of list for default value
+    # Default Option: adds 'Select year' or 'Select event' to beginning of list for default value
     def defop(array,var):
         if var == 'Y':
             array.insert(0,'---Select year---')
@@ -31,7 +31,7 @@ def setup(self):
 
     def pick_year(event):       
         if year_drop.get() == year[1]:
-            event_drop.config(value=defop(df_17_18.sheet_names,'E'))
+            event_drop.config(value=defop(xl_17_18.sheet_names,'E'))
             event_drop.current(0)
         if year_drop.get() == year[2]:
             event_drop.config(value=["No information available!"]) # write code to scan directory and check if this year's info is avaiable
@@ -40,7 +40,7 @@ def setup(self):
             event_drop.config(value=["No information available!"])
             event_drop.current(0)
         if year_drop.get() == year[4]:
-            event_drop.config(value=defop(df_20_21.sheet_names,'E'))
+            event_drop.config(value=defop(xl_20_21.sheet_names,'E'))
             event_drop.current(0)            
         if year_drop.get() == year[5]:
             event_drop.config(value=["No information available!"])
@@ -57,16 +57,29 @@ def setup(self):
     event_drop.current(0)
     event_drop.pack()
 
-    # adds a 0 to the first and last index of a list, needed because bar graph will be THICC otherwise
+    # Resize for bar: adds 0 to first & last index of list, needed because bar graph will be THICC otherwise
     def resize_for_bar(array):
         array.insert(0,0) # add a zero to beginning of list
         array.insert(2,0) # add a zero to end of the list
         return array
 
-    # function that returns an array containing a number of participants for a specified event 
-    def attendees_t():
-        pass
+    # Count Frequency: creates dictionary {'Target': # of appearances}
+    def CountFrequency(list):
+        # Creating an empty dictionary 
+        freq = {}
+        for item in list:
+            if item in freq: # if item is already in dictionary, add count
+                freq[item] += 1
+            else: # if item is new, add it to the dictionary
+                freq[item] = 1
+        return freq
 
+    # Attendees Total: returns an array containing a number of participants for an event 
+    def attendees_t(File):
+        col = list(File.columns)
+        
+
+    # Attendees Specific: returns an array of participants by class or participants by major for an event
     def attendees_s():
         pass
 
@@ -75,10 +88,10 @@ def setup(self):
         fig = Figure(figsize = (4, 5), dpi = 100) # sets parameters (size & dpi [dots per inch]) for the plot
 
         # function to be graphed
-        num_attend = [0,40,0] # THIS WILL USE VALUE RETURNED BY attendees() & resize_for_bar()
+        num_attend = resize_for_bar([40]) # THIS WILL USE VALUE RETURNED BY attendees_t() # Y-VALUES
         chart = fig.add_subplot(111) # adding the bar graph (code = 111) to the subplot
-        ind = np.arange(len(num_attend))
-        chart.bar(ind, num_attend, 0.8)
+        ind = np.arange(len(num_attend)) # X-VALUES, array with length of num_attend
+        chart.bar(ind, num_attend) # plot (x,y)
 
         # configure axes of bar graph
         chart.set_xticks(ind)
